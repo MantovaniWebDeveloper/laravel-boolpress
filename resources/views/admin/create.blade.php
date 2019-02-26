9fa2769beb93e17ca0e6ec6c2598d6c1a2717454
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-26 16:56:17 +0100

    creata struttura base del form post

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -2,10 +2,23 @@
 @section('content')
 <div class="container">
   <div class="row">
-    Aggiungi un post
+    <div class="col-12">
+      <h3>Aggiungi un post</h3>
+    </div>
   </div>
   <form class="" action="index.html" method="post">
-
+    <div class="form-group">
+      <input type="text" class="form-control" name="titolo" placeholder="titolo">
+    </div>
+    <div class="form-group">
+      <textarea class="form-control" name="content" rows="8" cols="80"></textarea>
+    </div>
+    <div class="form-group">
+      <select class="form-control" name="categorie">
+        <option value=""></option>
+      </select>
+    </div>
+    <input type="submit" class="btn btn-success col-12" name="salvaPost" value="Salva">
   </form>
 </div>
 @endsection
