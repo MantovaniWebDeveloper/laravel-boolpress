2b8a2996eac55a33db307b2c478b815a744ef456
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-27 10:05:34 +0100

    header creato per la parte utente

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -1,5 +1,11 @@
 <header>
   <div class="container">
-    sono header
+    <div class="row">
+      <div class="col-12">
+        <div class="wrapTitle">
+          <h2>BoolPress</h2>
+        </div>
+      </div>
+    </div>
   </div>
 </header>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.front')
 @section('content')
   @include('_partials.header')
+  <main class="py-4">
+    <div class="container">
+      ciao
+    </div>
+  </main>
 
 @endsection
